2556a8e3a520ca2bd2ecc82aa63bd8d37ad8de70
Capevace <capevace@gmail.com>
2017-01-19 00:11:32 +0100

    Fixed IFTTT module

--- system/modules/ifttt/index.js
+++ system/modules/ifttt/index.js
@@ -7,81 +7,59 @@ if (!config.modules.ifttt.webhookSecret) {
   console.logger.info(`Generated webhook secret '${uuid()}'`);
 }
 
-module.exports = {
-  info: {
-    name: 'IFTTT',
-    description: 'Enables the use of IFTTT Triggers.',
-    id: 'ifttt',
-    author: 'Lukas von Mateffy (@Capevace)',
-    type: 'virtual'
-  },
-  triggers: {
-    'ifttt.webhook': {
-      name: 'IFTTT triggered a webhoook',
-      sentence: 'IFTTT triggered a webhook'
-    }
-  },
-  actions: {
-    'ifttt.request-event': {
-      name: 'Set Switch to on',
-      sentence: 'set a switch to on',
-      arguments: {
-        name: {
-          type: 'string'
-        },
-        value1: {
-          type: 'string'
-        },
-        value2: {
-          type: 'string'
-        },
-        value3: {
-          type: 'string'
-        }
-      },
-      callback: data => {
-        if (!config.modules.ifttt || !config.modules.ifttt.apiKey) {
-          console.logger.error('Unable to send an event request to IFTTT. No API Key given.');
-          return;
-        }
+module.exports = builder => {
+  builder.triggers
+    .createTrigger('ifttt.webhook')
+    .setMeta('IFTTT triggered a webhoook', 'IFTTT triggered a webhook');
 
-        if (!data.name) {
-          console.logger.error('Unable to send an event request to IFTTT. No event name given.');
-          return;
-        }
-
-        const url = `https://maker.ifttt.com/trigger/${data.name}/with/key/${config.modules.ifttt.apiKey}`;
-        post(url, {
-          value1: data.value1,
-          value2: data.value2,
-          value3: data.value3
-        })
-          .then(function (response) {
-            if (response.status !== 200) {
-              console.logger.warn('Status code of IFTTT is not 200.', response);
-            } else {
-              console.logger.info(`IFTTT event '${data.name}' was triggered.`);
-            }
-          })
-          .catch(function (error) {
-            console.logger.error(`Error triggering IFTTT event '${data.name}'.`, error);
-          });
+  builder.actions
+    .createAction('ifttt.request-event')
+    .setMeta('Request IFTTT event', 'request IFTTT event')
+    .addArgument('name', 'string')
+    .addArgument('value1', 'string')
+    .addArgument('value2', 'string')
+    .addArgument('value3', 'string')
+    .setCallback(data => {
+      if (!config.modules.ifttt || !config.modules.ifttt.apiKey) {
+        console.logger.error('Unable to send an event request to IFTTT. No API Key given.');
+        return;
       }
-    }
-  },
-  routes: app => {
-    if (config.modules.ifttt.webhookSecret) {
-      app.post('/ifttt/webhook', (req, res) => {
-        const webhookSecret = req.query.key;
 
-        if (webhookSecret !== config.modules.ifttt.webhookSecret) {
-          res.sendStatus(401);
-          return;
-        }
+      if (!data.name) {
+        console.logger.error('Unable to send an event request to IFTTT. No event name given.');
+        return;
+      }
 
-        console.log(req.body);
-        res.sendStatus(200);
-      });
-    }
+      const url = `https://maker.ifttt.com/trigger/${data.name}/with/key/${config.modules.ifttt.apiKey}`;
+      post(url, {
+        value1: data.value1,
+        value2: data.value2,
+        value3: data.value3
+      })
+        .then(function (response) {
+          if (response.status !== 200) {
+            console.logger.warn('Status code of IFTTT is not 200.', response);
+          } else {
+            console.logger.info(`IFTTT event '${data.name}' was triggered.`);
+          }
+        })
+        .catch(function (error) {
+          console.logger.error(`Error triggering IFTTT event '${data.name}'.`, error);
+        });
+    });
+
+  if (config.modules.ifttt.webhookSecret) {
+    builder.routes
+        .post('/ifttt/webhook', (req, res) => {
+          const webhookSecret = req.query.key;
+
+          if (webhookSecret !== config.modules.ifttt.webhookSecret) {
+            res.sendStatus(401);
+            return;
+          }
+
+          console.log(req.body);
+          res.sendStatus(200);
+        });
   }
 };
